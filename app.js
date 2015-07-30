1ca82a8cdf753c17c01e47c4a1b0ae6dc8321132
Yanaël <b.yanael@gmail.com>
2015-07-30 22:51:24 +0200

    Add alerts view

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var alerts = require('./routes/alerts');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/alerts', alerts);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+// var https = require('https');
+// var querystring = require('querystring');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// var idMe = '433828_p7c1o80si28sgogw44ook4ococ8gsc0gswgcsocg0ccw8owgc';
+	// var accessToken = 'MmE5NDViMmJmOGRjNWIxYmYxZTRmM2E5OGUwODQwNTcwYTQyN2YwNzYxMTdkMGMyNWVkMmM4ZTNjOGJhYWRiOA';
+	//
+	// var options = {
+	// 	hostname: 'api.mention.net',
+	// 	path: '/api/accounts/' + idMe + '/alerts',
+	// 	method: 'GET',
+	// 	headers: {
+	// 		'Accept': 'application/json',
+	// 		'Accept-Language': 'fr',
+	// 		'Authorization': 'Bearer ' + accessToken
+	// 	}
+	// };
+	//
+	// var req = https.request(options, function(res) {
+	// 	res.setEncoding('utf8');
+	// 	res.on('data', function (chunk)
+	// 	{
+	// 		console.log(chunk);
+	// 		// req.write(chunk);
+	// 		// res.json(chunk);
+	// 	});
+	// 	res.once('end', function ()
+	// 	{
+	// 		// console.log(body.toString('utf8'));
+	// 	});
+	// });
+	//
+	// // req.end(new Buffer(JSON.stringify(requestData)));
+	// req.end();
+	// req.on('error', function(e) {
+	// 	console.error(e);
+	// });
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
